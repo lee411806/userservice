ed62b9d1a7a58603a80a9a738c096ab4bcb010b3
JDragon <lee411806@naver.com>
2025-01-09 20:10:19 +0900

    기본적인 mvc, dto 구현

--- src/main/java/com/example/springjwt/repository/UserRepository.java
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.springjwt.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Boolean existsByUsername(String username);
 }
