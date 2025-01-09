43178a8968345b5f2706f275ce39b652d8e1b10c
JDragon <lee411806@naver.com>
2025-01-09 20:35:01 +0900

    security filter 구현

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.springjwt.config;
 
 
+import com.example.springjwt.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //sercurity를 위한 configuration
 @Configuration
@@ -15,6 +19,21 @@ import org.springframework.security.web.SecurityFilterChain;
 /*이 애너테이션이 없으면 Spring Boot의 기본 보안 설정이 적용됨.
 이 애너테이션이 있으면, 정의된 보안 설정 클래스(SecurityConfig)를 기반으로 동작.*/
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
 
     // 비밀번호 단방향으로 암호화(복호화 안됨), 서버가 암호화 시켜서 저장해 놓는다.
@@ -50,6 +69,13 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+
 
         //세션 설정
         http
